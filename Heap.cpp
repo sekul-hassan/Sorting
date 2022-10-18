4dab4bd4058b43efb772da79d2ee22a06fc759ba
sekul hassan <68647401+sekul-hassan@users.noreply.github.com>
2022-10-19 00:01:54 +0600

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -1,41 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-int const n = 5;
-int arr[n] = {3, 6, 2, 5, 1};
-int l,r,m;
-void HEAP(int i)
-{
-    if(i==n)
-        return ;
-    l = 2*i+1;
-    r = 2*i+2;
-    if(l<n and arr[l]>arr[i]){
-        m = l;
-    }
-    else{
-        m = i;
-    }
-    if(r<n and arr[r]>arr[m]){
-        m = r;
-    }
-    if(m!=i){
-        swap(arr[i],arr[m]);
-    }
-    HEAP(i++);
 
+void MakeHeap(int arr[],int i,int n)
+{
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if(left<n and arr[largest]<arr[left])
+        largest = left;
+    if(right<n and arr[largest]<arr[right])
+        largest = right;
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        MakeHeap(arr,largest,n);
+    }
+}
+void HeapSort(int arr[],int n)
+{
+    for(int i=0;i<n/2-1;i++){
+        MakeHeap(arr,i,n);
+    }
+    for(int i=n-1;i>0;i--){
+        swap(arr[0],arr[i]);
+        MakeHeap(arr,0,i);
+    }
 }
 
-void Print()
+
+void Print(int arr[],int n)
 {
-    for(auto i:arr)
-        cout<<i<<" ";
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
     cout<<endl;
 }
 int main()
 {
-    int t = 1;
-    while(t--){
-        HEAP(0);
-        Print();
-    }
+    int n;
+    int arr[] = {3,6,2,5,1};
+    n = sizeof(arr)/sizeof(arr[0]);
+    HeapSort(arr,n);
+    Print(arr,n);
 }
